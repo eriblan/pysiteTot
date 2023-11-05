ad70a5c4323c06b1c702c2138de785985a9a88b5
Erik Blanning <erik.blanning@gmail.com>
2023-11-05 18:27:57 +0100

    Changed structure and changed from json to get_json()

--- PySite.API/flask_app/routes.py
+++ PySite.API/flask_app/routes.py
@@ -26,6 +26,35 @@ def handle_users():
             return jsonify({"message": "could not update users"}), 500
 
 
+# @app.route("/api/users/<user_id>", methods=["GET", "PUT", "DELETE"])
+# def handle_user(user_id):
+#     """
+#     This function takes an ID as dynamic URL parameter, for example 0
+#     Note:
+#     - GET method is only available for debugging purposes, to test for a specific user
+#     open the browser with for example http://localhost:5000/api/users/0 to see the data
+
+#     - The assignment is to implement the PUT and DELETE methods for the specific ID, see assignment for specific instructions
+#     """
+#     if user_id in users:
+#         if request.method == "GET":
+#             return jsonify(users[user_id])
+        
+#         if request.method == "DELETE":
+#             del users[user_id]
+#             return jsonify({"message": "User deleted"}), 200
+
+#         if request.method == "PUT":
+#             if (request.json['name']):
+#                 users[user_id]['name'] = request.json['name']
+#                 return jsonify({"message": "User updated"}), 200
+#             else:
+#                 return jsonify({"message": "Empty username"}), 400
+
+#     else:
+#         return jsonify({"message": "User not found"}), 404
+
+
 @app.route("/api/users/<user_id>", methods=["GET", "PUT", "DELETE"])
 def handle_user(user_id):
     """
@@ -36,20 +65,21 @@ def handle_user(user_id):
 
     - The assignment is to implement the PUT and DELETE methods for the specific ID, see assignment for specific instructions
     """
-    if user_id in users:
-        if request.method == "GET":
-            return jsonify(users[user_id])
-        
-        if request.method == "DELETE":
-            del users[user_id]
-            return jsonify({"message": "User deleted"}), 200
-
-        if request.method == "PUT":
-            if (request.json['name']):
-                users[user_id]['name'] = request.json['name']
-                return jsonify({"message": "User updated"}), 200
-            else:
-                return jsonify({"message": "Empty username"}), 500
-
-    else:
+    if user_id not in users:
         return jsonify({"message": "User not found"}), 404
+
+    if request.method == "GET":
+        return jsonify(users[user_id])
+
+    if request.method == "DELETE":
+        del users[user_id]
+        return jsonify({"message": "User deleted"}), 200
+
+    if request.method == "PUT":
+        request_data = request.get_json()
+        name = request_data.get('name')
+        if name:
+            users[user_id]['name'] = name
+            return jsonify({"message": "User updated"}), 200
+        else:
+            return jsonify({"message": "Empty username"}), 400
